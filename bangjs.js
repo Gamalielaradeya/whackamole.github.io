bcfad1a9a1e5caa31a48aee9c613e88dd2572d94
Gamaliel Aradeya <[Gamalielshroud@gmail.com]>
2023-05-25 15:37:33 +0700

    update button mulai and reset

--- bangjs.js
+++ bangjs.js
@@ -1,10 +1,22 @@
 const container = document.querySelector('.container');
 const allMole = document.querySelectorAll('.item');
 const mulaigem = document.getElementById('mulaigem');
+const resetgem = document.getElementById('resetgem');
 let mulai,waktuMulai,waktu = 20,skor = 0;
 
+
 const hitungwaktu = document.getElementById('time-count');
+
 const hitungskor = document.getElementById('score-count');
+
+resetgem.addEventListener('click',function(){
+skor=0;
+hitungskor.innerHTML = skor;
+waktu=20;
+hitungwaktu.innerHTML = waktu;
+clearInterval(mulai);
+clearInterval(waktuMulai);
+});
 
 container.addEventListener('click', function (e) {
   if (e.target.classList.contains('mole-clicked')) {
@@ -26,6 +38,7 @@ container.addEventListener('click', function (e) {
 
 mulaigem.addEventListener('click', () => {
   // total game time is 20 seconds
+  resetgem.click();
   waktuMulai = setInterval(() => {
     hitungwaktu.innerHTML = waktu;
     waktu--;
