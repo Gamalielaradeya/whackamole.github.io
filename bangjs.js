298ab9d7d29b0a48728229bc798cc5d4026d6570
chiiaaruan <chiakampus@gmail.com>
2023-05-25 14:41:13 +0700

    mobile web size

---
+++ bangjs.js
@@ -0,0 +1,60 @@
+const container = document.querySelector('.container');
+const allMole = document.querySelectorAll('.item');
+const mulaigem = document.getElementById('mulaigem');
+let mulai,waktuMulai,waktu = 20,skor = 0;
+
+const hitungwaktu = document.getElementById('time-count');
+const hitungskor = document.getElementById('score-count');
+
+container.addEventListener('click', function (e) {
+  if (e.target.classList.contains('mole-clicked')) {
+    skor++
+    hitungskor.innerHTML = skor;
+
+    const bush = e.target.parentElement.previousElementSibling;
+
+    let text = document.createElement('span');
+    text.setAttribute('class', 'whack-text');
+    text.innerHTML = 'Whack!';
+    bush.appendChild(text);
+
+    setTimeout(() => {
+      text.remove();
+    }, 300);
+  }
+})
+
+mulaigem.addEventListener('click', () => {
+  // total game time is 20 seconds
+  waktuMulai = setInterval(() => {
+    hitungwaktu.innerHTML = waktu;
+    waktu--;
+  }, 1000);
+
+  mulai = setInterval(() => {
+    showMole();
+  }, 600);
+});
+
+function showMole() {
+  if (waktu <= 0) {
+    clearInterval(mulai);
+    clearInterval(waktuMulai);
+    hitungwaktu.innerHTML = '0';
+  }
+  let moleToAppear = allMole[getRandomValue()].querySelector('.mole');
+  moleToAppear.classList.add('mole-appear');
+  hideMole(moleToAppear);
+}
+
+function getRandomValue() {
+  let rand = Math.random() * allMole.length;
+  return Math.floor(rand);
+}
+
+// hide Mole
+function hideMole(moleToAppear) {
+  setTimeout(() => {
+    moleToAppear.classList.remove('mole-appear');
+  }, 1000);
+}
